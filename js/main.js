0e8643814c59fe472949bba308af473bd4eea833
Own3Dqq <own3dqq@gmail.com>
2022-08-20 16:04:05 +0300

    fix hero slider section

--- js/main.js
+++ js/main.js
@@ -1,23 +1,34 @@
 /* Slick slider (Hero) */
 $(document).ready(function () {
 	$('.hero-slider__list').slick({
-		arrows: false,
+		arrows: true,
 		infinite: false,
-		// autoplay: true,
-		speed: 300,
+		autoplay: true,
+		dots: true,
+		speed: 500,
 		fade: true,
 		cssEase: 'linear',
 		slidesToShow: 1,
 		slidesToScroll: 1,
+		prevArrow: '.hero-slider__prev',
+		nextArrow: '.hero-slider__next',
+		dotsClass: 'hero-slider__wrapper',
+		appendDots: '.hero-slider__switchers',
+		slickPrev: '.hero-slider__prev',
+		slickPrev: '.hero-slider__next',
 	});
 });
 
+const currentSlide = document.querySelector('.current');
+console.log(currentSlide);
+
+/* Works Slider */
 // $(document).ready(function () {
-// 	$('.slider__list').slick({
+// 	$('.works-slider__list').slick({
 // 		arrows: false,
 // 		infinite: false,
-// 		autoplay: true,
-// 		speed: 300,
+// 		autoplay: false,
+// 		// speed: 300,
 // 		fade: true,
 // 		cssEase: 'linear',
 // 		slidesToShow: 1,
