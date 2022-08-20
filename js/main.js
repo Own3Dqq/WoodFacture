f9515721277d96e5cece615ea333f09ab8624f20
Own3Dqq <own3dqq@gmail.com>
2022-08-20 12:26:41 +0300

    refactor hero section

--- js/main.js
+++ js/main.js
@@ -1,39 +1,16 @@
-// const swiper = new Swiper('.swiper', {
-// 	// Optional parameters
-// 	direction: 'vertical',
-// 	loop: true,
-
-// 	// If we need pagination
-// 	pagination: {
-// 		el: '.swiper-pagination',
-// 	},
-
-// 	// Navigation arrows
-// 	navigation: {
-// 		nextEl: '.swiper-button-next',
-// 		prevEl: '.swiper-button-prev',
-// 	},
-
-// 	// And if we need scrollbar
-// 	scrollbar: {
-// 		el: '.swiper-scrollbar',
-// 	},
-// });
-
 /* Slick slider (Hero) */
-// $(document).ready(function () {
-// 	$('.hero-slider__list').slick({
-// 		arrows: false,
-// 		infinite: false,
-// 		autoplay: true,
-// 		speed: 300,
-// 		fade: true,
-// 		// he
-// 		cssEase: 'linear',
-// 		slidesToShow: 1,
-// 		slidesToScroll: 1,
-// 	});
-// });
+$(document).ready(function () {
+	$('.hero-slider__list').slick({
+		arrows: false,
+		infinite: false,
+		// autoplay: true,
+		speed: 300,
+		fade: true,
+		cssEase: 'linear',
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	});
+});
 
 // $(document).ready(function () {
 // 	$('.slider__list').slick({
